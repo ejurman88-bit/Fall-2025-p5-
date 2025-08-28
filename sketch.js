5b63a5290f454cb177e0e74321bea8df9ff82f53
ejurman88-bit <ethanjurman88@gmail.com>
2025-08-28 11:50:56 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,105 @@
+function setup() {
+  
+  createCanvas(windowWidth, windowHeight);
+}
+
+
+
+function draw() {
+ 
+  noStroke()
+  background(130,200,229);
+    rect(0, 500, 700, 100)
+
+  fill (255, 223, 34)
+ ellipse(50, 50, 90);
+  
+  line(350, 50, 400, 50)
+  
+  fill (29, 191, 17)
+  rect(0, 500, 700, 100)
+  
+ textSize(300)
+  
+  text('🌈', 400, 470);
+  
+  stroke('white');
+  strokeWeight(1);
+  
+ 
+  
+  
+  fill(124,10,2)
+   square(280, 420, 100);
+  
+  fill(128,10,2)
+  rect(310, 480, 20, 40)
+  
+  fill(128,10,2)
+  rect(330, 480, 20, 40)
+  
+triangle(280, 420, 380, 420, 330, 355);
+  
+  square (317,395,25)
+  
+  
+   line(310, 480, 330, 520);
+  line(310, 520, 330, 480);
+  
+  line(330, 480, 350, 520);
+   line(330, 520, 350, 480);
+  
+  line(317, 395, 342, 420)
+  line(317, 420, 342, 395)
+  
+  fill(255,255,255)
+  noStroke()
+  ellipse(280, 70, 45)
+  ellipse(350, 70, 55)
+  ellipse(310, 70, 60)
+  
+  ellipse(430, 180, 55)
+  ellipse(460, 180, 65)
+  ellipse(510, 190, 60)
+  
+  ellipse(50, 260, 50)
+  ellipse(90, 265, 60)
+  ellipse(130, 255, 55)
+  
+ellipse(100, 500, 20)
+  ellipse(108, 500, 20)
+  ellipse(112, 500, 20)  
+  
+  fill(0)
+  
+  ellipse(115, 500, 15)
+
+ stroke('black');
+  strokeWeight(2);
+
+line(115, 500, 115, 515)
+line(110, 500, 110, 515)
+
+line(105, 500, 105, 515)  
+line(100, 500, 100, 515)
+  
+    fill(255,255,255)
+  noStroke()
+  
+  ellipse(100, 500, 20)
+  ellipse(108, 500, 20)
+  ellipse(112, 500, 20) 
+  
+    fill(0)
+  
+  ellipse(115, 500, 15)
+
+   textSize(50)
+  
+  text('🐑', 200, 500);
+  text('🐸', 100, 500);
+}
+
+
+
+
